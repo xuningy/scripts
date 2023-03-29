0f30cfe04ed85dc71cc43e50abde32bd6287d73c
Xuning Yang <xuningy@gmail.com>
2023-03-29 14:56:56 -0700

    update shortcuts.sh

--- shortcuts.sh
+++ shortcuts.sh
@@ -3,6 +3,23 @@
 # A set of scripts for projects that I work on. run the following line to access these functions
 # echo "source ~/scripts/shortcuts.sh" >> ~/.bashrc
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LTGRAY='\033[0;37m'
+DKGRAY='\033[1;30m'
+LTRED='\033[1;31m'
+LTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LTBLUE='\033[1;34m'
+LTPURPLE='\033[1;35m'
+LTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
 # End the current working session
 bye() {
 
@@ -16,33 +33,35 @@ bye() {
 }
 
 cuda-versions() {
-    echo "=========================================================================="
-    echo "CUDA Toolkit Version (nvcc --version) [Needs to be lower than Driver version]" 
-    echo "------------------------------------------------"
+    printf "==============================================================================\n"
+    echo -e "${LTCYAN}CUDA Toolkit Version (nvcc --version) [Needs to be lower than Driver version]${NC}" 
     nvcc --version
+    echo " "
 
-    echo "=========================================================================="
-    echo "Cuda GPU Driver Version (nvidia-smi)": 
-    echo "------------------------------------------------"
+    printf "==============================================================================\n"
+    echo -e "${LTCYAN}Cuda GPU Driver Version (nvidia-smi):${NC}" 
     nvidia-smi
+    echo " "
 }
 
 glibc-version() {
-    echo "=========================================================================="
-    echo "GLIBC Version (ldd --version)": 
-    echo "------------------------------------------------"  
-    ldd --version
+    printf "==============================================================================\n"
+    echo -e "${LTCYAN}GLIBC Version (ldd --version):${NC}" 
+    ldd --version | head -n1
+    echo " "
 }
 
 versions() {
     cuda-versions 
     glibc-version
 
-    echo "========================================================"
-    echo "Python Version (python3 version)": 
+    printf "========================================================\n"
+    echo -e "${LTCYAN}Python Version (python3 version):${NC}" 
     python3 --version
+    echo " "
 
-    echo "========================================================"
-    echo "Git Version (git --version)": 
+    printf "========================================================\n"
+    echo -e "${LTCYAN}Git Version (git --version):${NC}" 
     git --version
+    echo " "
 }
