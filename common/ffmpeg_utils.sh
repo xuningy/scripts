a12c628dbf34c907c1b7681de87aed5bed07e6e4
Xuning Yang <xuningy@gmail.com>
2024-12-16 10:22:00 -0800

    Add side-by-side ffmpeg and add video to gif

--- common/ffmpeg_utils.sh
+++ common/ffmpeg_utils.sh
@@ -33,6 +33,44 @@ ffmpeg_speed() {
     ffmpeg -i ${fullfile} -filter:v setpts=PTS/${speed} "${directory}/${filename}_${speed}X.${extension}"
 }
 
+ffmpeg_video_to_gif() {
+        if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+        echo -e "Usage: ffmpeg_video_to_gif [filename_with_ext] [fps] [scale]"
+        return
+    fi
+    fullfile=$1
+    fps=$2
+    scale=$3
+
+    filename=$(basename -- "$fullfile")
+    directory=$(dirname -- "$fullfile")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    # ffmpeg -i ${fullfile} "${directory}/${filename}_cut_${duration}s.${extension}"
+    ffmpeg -i ${fullfile} -vf "fps=${fps},scale=${scale}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+    -loop 0 "${directory}/${filename}_fps${fps}.gif"
+} 
+
+ffmpeg_three_videos_side_by_side() {
+    if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+        echo -e "Usage: ffmpeg_three_videos_side_by_side [file_1] [file_2] [file_3] [output_file_name_no_ext]"
+        return
+    fi
+    
+    fullfile_1=$1
+    fullfile_2=$2
+    fullfile_3=$3
+    output_filename=$4
+
+    filename=$(basename -- "$fullfile_1")
+    directory=$(dirname -- "$fullfile_1")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    ffmpeg -i ${fullfile_1} -i ${fullfile_2} -i ${fullfile_3} -filter_complex "[1:v][0:v]scale2ref=oh*mdar:ih[1v][0v];[2:v][0v]scale2ref=oh*mdar:ih[2v][0v];[0v][1v][2v]hstack=3,scale='2*trunc(iw/2)':'2*trunc(ih/2)'" "${directory}/${output_filename}.mp4"
+}
+
 ffmpeg_cut() {
     if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
         echo -e "Usage: ffmpeg_cut [filename_with_ext] [start_time (s)] [duration (s)]"
@@ -52,7 +90,7 @@ ffmpeg_cut() {
 
 ffmpeg_img_to_video() {
     if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-        echo -e "Usage: ffmpeg_gif_to_video [pattern](e.g., rgb_%d.png) [start_number](OPTIONAL, 0 otherwise) [output_filename](OPTIONAL)"
+        echo -e "Usage: ffmpeg_img_to_video [pattern](e.g., rgb_%d.png) [start_number](OPTIONAL, 0 otherwise) [output_filename](OPTIONAL)"
         return
     fi
 
